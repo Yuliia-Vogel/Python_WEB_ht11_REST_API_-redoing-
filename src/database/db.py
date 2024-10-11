4f0502b2b9896890405d9f06efd9ae6a93fa43f0
Sergstm <sstm@ukr.net>
2024-10-11 21:01:36 +0300

    .env-file-added

--- src/database/db.py
+++ src/database/db.py
@@ -1,7 +1,21 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:qwerty123@localhost:5433/hw11_ver2_base"
+load_dotenv() # завантажуються дані з файлу .env 
+
+postgres_user = os.getenv("POSTGRESQL_USER")
+postgres_password = os.getenv("POSTGRESQL_PASS")
+postgres_db_name = os.getenv("POSTGRESQL_DB_NAME")
+
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:qwerty123@localhost:5433/hw11_ver2_base"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{postgres_user}:{postgres_password}@localhost:5433/{postgres_db_name}"
+
+# Перевірка наявності даних для підключення
+if not all([postgres_user, postgres_password, postgres_db_name]):
+    raise ValueError("Потрібно задати всі змінні середовища для підключення до бази даних.")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
